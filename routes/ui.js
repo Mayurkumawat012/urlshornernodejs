2f4e35bab5e1bee5b02165c5c896612806f742d0
Mayur Kumawat <mayurkumawat012@gmail.com>
2020-05-04 15:54:01 +0530

    dynamic api in html page

--- routes/ui.js
+++ routes/ui.js
@@ -1,16 +1,44 @@
 var express = require('express');
 var router = express.Router();
-// var db = require('../htmlpage/index.html');
-/* GET users listing. */
+var fs = require('fs');
+var handlebars = require('handlebars');
 const path = require('path');
+var {hostname} = require('../config/cred');
 
 router.get('/home', function(req, res, next) {
-    // res.writeHead(200, {
-    //     'Content-Type': 'text/html'
-    // });
-    // res.write(``);
-    // res.end();
-    res.sendFile(path.join(__dirname+'/../htmlpage/index.html'));
+    var readHTMLFile = function(path, callback) {
+        fs.readFile(path, {encoding: 'utf-8'}, function (err, html) {
+          if (err) {
+            throw err;
+            callback(err);
+          }
+          else {
+            callback(null, html);
+          }
+        });
+      };
+
+    readHTMLFile('./htmlpage/index.html', function(err, html) {
+        if(err)
+        {
+            res.status(500).send({"status":"failure","error":"Error reading template"});
+        }
+        else
+        {
+            var template = handlebars.compile(html);
+            var replacements = {
+              hostname: hostname
+            };
+    
+            var htmlToSend = template(replacements);
+    
+            
+            res.send(htmlToSend);
+            
+        }
+        
+    }); 
+    
 
 });
 
